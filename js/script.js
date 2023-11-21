dd0253b664c0ce3f3bea55b496c0c777b7cd07e9
gbsr <anders.jakobsen.hofsten@gmail.com>
2023-11-21 13:59:02 +0100

    initial setup for js functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-import {words} from './svenska-ord.js';
-
-
-
 const btnStartView = document.getElementById("btn-start-view");
 
 btnStartView.addEventListener("click", doStuff);
 
 console.log('HI');
-// console.log(btnStartView);
+console.log(btnStartView);
+
 function doStuff() {
+	console.log('WE DID STUFF!');
 }
+
+
+
+
+
+
