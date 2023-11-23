0089c6d5e8754c635924cabe457b60154d13a543
Julius Kaljo <julius.kaljo@hotmail.se>
2023-11-23 15:48:47 +0100

    ska hämta ner rätt verwsion av div, addade testdata till score view

--- js/score-view.js
+++ js/score-view.js
@@ -1,4 +1,4 @@
-// gå igenom med anders om vilka variablar jag ska använda
+//  gå igenom med anders om vilka variablar jag ska använda
 
 // VARIABLER = userObject (.wordLenght, .date, .time, .win, .nrWin), userOBject.currentScore, userScore som tillfällig
 
@@ -13,12 +13,14 @@
 
 // hanterar datan 
 function updateScore (score) {
-	return data
+	let updatedScore = score + 10
+	return updatedScore
 	}
 	
 	// hanterar datan
 	function getScore (userObject) {
-	return score
+		let calculatedScore = userObject.win ? userObject.wordLength * 5
+		return score
 	}
 	
 	// uppdaterar dom
@@ -26,7 +28,51 @@ function updateScore (score) {
 		
 	}
 
+	const userObject = {
+		userName: null,
+		win: null,
+		lost: null,
+		date: null,
+		time: null,
+		wordLength: null,
+		numberOfFailedGuesses: null
+	};
 	
+	// Simulerad data för startView och gameView
+	let startView = {
+		player: "Alice",
+		date: "2023-11-24",
+		time: "15:45",
+	};
+	
+	let gameView = {
+		correctLetters: 5,
+		incorrectLetters: 3,
+		wordToGuess: "EXAMPLE",
+	};
+	
+	// Skapa en funktion för att generera testdata och uppdatera userObject
+	function genereraHangmanTestdata() {
+		// Skapa en unik id baserad på tidsstämpel
+		const id = new Date().getTime().toString();
+	
+		// Uppdatera userObject med simulera användarens poäng för hänga gubbe-spelet
+		userObject.userName = startView.player;
+		userObject.date = startView.date;
+		userObject.time = startView.time;
+		userObject.win = gameView.correctLetters === gameView.wordToGuess.length;
+		userObject.lost = !userObject.win;
+		userObject.wordLength = gameView.wordToGuess.length;
+		userObject.numberOfFailedGuesses = gameView.incorrectLetters;
+	
+		// Du kan också spara userObject i localStorage om det behövs
+		localStorage.setItem('userObject', JSON.stringify(userObject));
+	
+		console.log("Testdata för hänga gubbe har genererats och uppdaterat userObject.");
+	}
+	
+	// Anropa funktionen för att generera testdata för hänga gubbe
+	genereraHangmanTestdata();
 
 	
 	  
