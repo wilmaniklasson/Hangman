494446dd0d52c89055d1c2bef3343b97994593dd
wilmaniklasson <wilma.niklasson2@hotmail.se>
2023-11-23 11:46:27 +0100

    Modal box

--- js/start-view.js
+++ js/start-view.js
@@ -1,25 +1,25 @@
+//hämtar element
+const startViewSection = document.querySelector('.start-view-section');
 const userNameInput = document.querySelector('#user-name-input');
 const btnStartGame = document.querySelector('#start-game')
-const startViewSection = document.querySelector('.start-view-section');         
-const gameViewSection = document.querySelector('.game-view-section');          
-const scoreViewSection = document.querySelector('.score-view-section');   
-const userObject = {};
-
-
+const gameViewSection = document.querySelector('.game-view-section');
+const scoreViewSection = document.querySelector('.score-view-section');
+const modal = document.querySelector('#Modal');  
+//Skapar ett objet för användar info
+const userObject = {};     
+       
+/* Start game btn: sparar användar namn och skapar ett objekt. Om vi fått ett värde från input blir start view section dåld och game view section visas om inte vissar vi en modal ruta så att user name input blir required. */
 btnStartGame.addEventListener('click', function(event) {
     event.preventDefault();
     saveInput();
     
-
-
     if (userObject.userName) {
         startViewSection.classList.add('hidden');
         gameViewSection.classList.remove('hidden'); // Visa spelvyn
         scoreViewSection.classList.add('hidden');
         console.log(userObject);
     } else {
-        ("Vänligen fyll i ditt användarnamn innan du startar spelet.");
-        // Gör en Modal Box här 
+        modal.style.display = 'block';
     }
     
 });
@@ -33,6 +33,8 @@ function saveInput() {
     }
 }
 
+
+//Skapar fler objekt för användar info 
 userObject.wordLength = 0;
 userObject.date = 0; //Date().toLocaleDateString();
 userObject.time = 0; //Date().toLocaleTimeString();
@@ -43,5 +45,32 @@ userObject.nrLost = 0;
 userObject.numberOfFailedGuesses = 0;
 
 
-/*localStorage.setItem('userObject', JSON.stringify(userObject))*/
+//Låter användaren klicka utaför modal rutan för att stänga rutan.
+const openModalBtn = document.getElementById('openModalBtn');
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+});
 
+//funktion för att stänga modal med "X"
+const closeModalBtn = document.getElementById('closeModalBtn');
+closeModalBtn.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+localStorage.setItem('userObject', JSON.stringify(userObject))
+
+
+
+
+
+/* Så här hämtar vi och skickar upp värden till localStorage */
+//hämta localStorage   
+/* userObject = JSON.parse(localStorage.getItem('userObject')) || {}; /*
+
+//ändra värderna
+/* userObject.wordLength = 10; */
+
+// Spara uppdaterad användardata till localStorage
+/* localStorage.setItem('userObject', JSON.stringify(userObject)); */
