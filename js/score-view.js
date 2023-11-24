ac0be12b3dc0d54556c193728a625bbb89dc8288
Julius Kaljo <julius.kaljo@hotmail.se>
2023-11-24 09:53:23 +0100

    big change w testdata in score-view

--- js/score-view.js
+++ js/score-view.js
@@ -1,86 +1,94 @@
-//  gå igenom med anders om vilka variablar jag ska använda
-
-// VARIABLER = userObject (.wordLenght, .date, .time, .win, .nrWin), userOBject.currentScore, userScore som tillfällig
-
-// hämta resultat ( antal fel/rätt, namn och kanske tid ) från start-view och game-view (lokalStorage)
-
-// skapa en funktion för att lägga till resultat till localStorage (det kommer göras i game-view)
-
-// skapa funktion som sparar i localStorage
-
-// uppdatera scoreboarden efter varje spel
-
-
-// hanterar datan 
-function updateScore (score) {
-	let updatedScore = score + 10
-	return updatedScore
-	}
-	
-	// hanterar datan
-	function getScore (userObject) {
-		let calculatedScore = userObject.win ? userObject.wordLength * 5
-		return score
-	}
-	
-	// uppdaterar dom
-	function updateDisplay (data) {
-		
-	}
-
-	const userObject = {
-		userName: null,
-		win: null,
-		lost: null,
-		date: null,
-		time: null,
-		wordLength: null,
-		numberOfFailedGuesses: null
-	};
-	
-	// Simulerad data för startView och gameView
-	let startView = {
-		player: "Alice",
-		date: "2023-11-24",
-		time: "15:45",
-	};
-	
-	let gameView = {
-		correctLetters: 5,
-		incorrectLetters: 3,
-		wordToGuess: "EXAMPLE",
-	};
-	
-	// Skapa en funktion för att generera testdata och uppdatera userObject
-	function genereraHangmanTestdata() {
-		// Skapa en unik id baserad på tidsstämpel
-		const id = new Date().getTime().toString();
-	
-		// Uppdatera userObject med simulera användarens poäng för hänga gubbe-spelet
-		userObject.userName = startView.player;
-		userObject.date = startView.date;
-		userObject.time = startView.time;
-		userObject.win = gameView.correctLetters === gameView.wordToGuess.length;
-		userObject.lost = !userObject.win;
-		userObject.wordLength = gameView.wordToGuess.length;
-		userObject.numberOfFailedGuesses = gameView.incorrectLetters;
-	
-		// Du kan också spara userObject i localStorage om det behövs
-		localStorage.setItem('userObject', JSON.stringify(userObject));
-	
-		console.log("Testdata för hänga gubbe har genererats och uppdaterat userObject.");
-	}
-	
-	// Anropa funktionen för att generera testdata för hänga gubbe
-	genereraHangmanTestdata();
-
-	
-	  
+// Deklarera userObject
+const userObject = {
+    userName: null,
+    win: null,
+    lost: null,
+    date: null,
+    time: null,
+    wordLength: null,
+    numberOfFailedGuesses: null,
+    currentScore: null  // Lägg till currentScore här om du använder det
+};
 
 
-		// resultatet från tidigare omgångar:
-		// spelarens namn,
-		// antalet felaktiga gissningar,
-		// ordets längd,
-	    // datum+tid för omgången,
-		// och om man vann eller förlorade.
+// Simulerad data för startView och gameView
+let startView = {
+    player: "Alice",
+    date: "2023-11-24",
+    time: "15:45",
+};
+
+
+let gameView = {
+    correctLetters: 7,
+    incorrectLetters: 2,
+    wordToGuess: "EXAMPLE",
+};
+
+// Funktion för att generera testdata och uppdatera userObject
+function genereraHangmanTestdata(startView, gameView) {
+    // Skapa en unik id baserad på tidsstämpel
+    const id = new Date().getTime().toString();
+
+    // Uppdatera userObject med simulera användarens poäng för hänga gubbe-spelet
+    userObject.userName = startView.player;
+    userObject.date = startView.date;
+    userObject.time = startView.time;
+    userObject.win = gameView.correctLetters === gameView.wordToGuess.length;
+    userObject.lost = !userObject.win;
+    userObject.wordLength = gameView.wordToGuess.length;
+    userObject.numberOfFailedGuesses = gameView.incorrectLetters;
+
+    // Spara userObject i localStorage
+    localStorage.setItem('userObject', JSON.stringify(userObject));
+
+    console.log("Testdata för hänga gubbe har genererats och uppdaterat userObject.");
+}
+
+// Anropa funktionen för att generera testdata för hänga gubbe
+genereraHangmanTestdata(startView, gameView);
+
+// Hanterar datan 
+function updateScore(score) {
+    let updatedScore = score + 100;
+    return updatedScore;
+}
+
+// Hanterar datan
+function getScore(userObject) {
+    let calculatedScore = userObject.win ? userObject.wordLength * 5 : userObject.numberOfFailedGuesses * 2;
+    return calculatedScore;
+}
+
+function updateDisplay(data) {
+    const resultDiv = document.getElementById('resultDiv');
+
+    // Skapa en div för användarinformation
+    const userInfoDiv = document.createElement('userInfoDiv');
+    userInfoDiv.innerHTML = `
+        <h2>Användare: ${userObject.userName}</h2>
+        <tr>Datum: ${userObject.date}</tr>
+        <tr>Tid: ${userObject.time}</tr>
+        <tr>Antal korrekta bokstäver: ${gameView.correctLetters}</tr>
+        <tr>Antal felaktiga gissningar: ${gameView.incorrectLetters}</tr>
+        <tr>Ordet att gissa: ${gameView.wordToGuess}</tr>
+        <tr>Antal poäng: ${data}</tr>
+    `;
+
+    // Rensa resultDiv och lägg till userInfoDiv
+    resultDiv.innerHTML = '';
+    resultDiv.appendChild(userInfoDiv);
+}
+
+
+// Hämta testdata från localStorage
+const storedUserData = JSON.parse(localStorage.getItem('userObject'));
+
+// Använd funktionerna med testdatan
+const initialScore = storedUserData.currentScore; // Antag att currentScore är det du vill uppdatera
+const updatedScore = updateScore(initialScore);
+const calculatedScore = getScore(storedUserData);
+
+// Uppdatera gränssnittet med de nya poängen
+updateDisplay(updatedScore);
+updateDisplay(calculatedScore);
