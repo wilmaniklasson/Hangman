d9f84e12a4c08b62a30f0a6c8826756f243fd056
gbsr <anders.jakobsen.hofsten@gmail.com>
2023-11-26 16:54:21 +0100

    Render functions for word + alphabet display. Fixed typo in alfabet. Added sections in HTML.

---
+++ js/game-view.js
@@ -0,0 +1,195 @@
+import { alfabetet } from './svenska-ord.js';
+import { words } from './svenska-ord.js';
+
+// display containers
+const gameViewSection = document.querySelector('.game-view-section');
+const guessContainer = document.querySelector('.guess-content');
+const letterContainer = document.createElement('div');
+letterContainer.className = 'letter-container';
+
+
+// gameplay variables
+let visibleWord = '';
+let input = 'A';
+
+const uData = JSON.parse(localStorage.getItem('userObject'));
+const numberOfLetters = 10;
+const currentWord = pickNewWord(numberOfLetters);
+
+
+// ------------------------------------------------------------------------ //
+
+// game loop
+renderAlphabet(alfabetet);
+renderWord(currentWord);
+
+// game logic functions
+
+function renderAlphabet(alfabetet) {
+
+	let letterContainer = createNewElement('div', 'letter-container');
+
+	// loop through each char in currentWord, append the text node to our newly created element
+	for (let char of alfabetet) {
+
+		let character = createNewElement('div', 'character');
+		let textNode = document.createTextNode(char.toUpperCase());
+		character.appendChild(textNode);
+		letterContainer.appendChild(character);
+	}
+
+	gameViewSection.append(letterContainer);
+}
+
+
+function renderWord(currentWord) {
+
+	let wordContainer = createNewElement('div', 'word-container');
+	// loop through each char in currentWord, append the text node to our newly created element
+	for (let char of currentWord) {
+
+		let word = createNewElement('div', 'word');
+		let textNode = document.createTextNode(char.toUpperCase());
+		word.appendChild(textNode);
+		wordContainer.appendChild(word);
+	}
+
+	gameViewSection.append(wordContainer);
+}
+
+
+// helper functions
+function createNewElement(typeOfElement, className) {
+	let newElement = document.createElement(typeOfElement);
+	newElement.className = className;
+	return newElement;
+}
+
+function pickNewWord(numberOfLetters) {
+
+	// find the ten letter letters from the list by filtering
+	const currentWordList = words.filter(word => word.length === numberOfLetters);
+	// const userObject = localStorage.getItem('userObject');
+
+	/* generate random float based on length of currentWordList,
+	then floor it to an integer.
+	This is so we have a whole number to use for selecting from the array :)
+	*/
+	const newWord = Math.floor(Math.random() * currentWordList.length);
+
+	// then return the word that we got from the array
+	return currentWordList[newWord];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function displayWord(currentWord) {
+
+// 	let wordContainer = createNewElement('div', 'word-container');
+// 	// loop through each char in currentWord, append the text node to our newly created element
+// 	for (let char of currentWord) {
+
+// 		let word = createNewElement('div', 'word');
+// 		// let word = document.createElement('div');
+// 		// word.className = 'word';
+// 		let textNode = document.createTextNode(char.toUpperCase());
+// 		word.appendChild(textNode);
+// 		// wordContainer.appendChild(word);
+// 	}
+
+// 	// gameViewSection.append(wordContainer);
+// 	console.log(currentWord);
+// }
+
+
+// console.log('Input: ' + input);
+
+/* first we append the char to their own divs,
+and then when exiting the loop we append those to the word container
+*/
+// function displayRandomWord(currentWord, element, className) {
+
+// 	createNewElement(element, className);
+
+// }
+
+
+
+
+// gameViewSection.append(letterContainer);
+// console.log(currentWord);
+
+// function createEdddlement(element, className) {
+// 	guessContent.classNAme = className;
+
+// 	for (let char of currentWord) {
+// 		let textNode;
+
+// 		if (char.toUpperCase() === input.toUpperCase()) {
+// 			visibleWord += char;
+// 			textNode = document.createTextNode(char.toUpperCase());
+// 		}
+
+// 		else {
+// 			visibleWord += '_';
+// 			textNode = document.createTextNode('_');
+// 		}
+
+// 		let guessed = document.createElement('div');
+// 		guessed.className = 'word';
+// 		guessed.appendChild(textNode);
+// 		guessContent.appendChild(guessed);
+
+// 	}
+// 	guessContainer.append(guessContent);
+
+// }
+
+// function guess() {
+
+
+// 		if (char.toUpperCase() === input.toUpperCase()) {
+// 			visibleWord += char;
+// 			textNode = document.createTextNode(char.toUpperCase());
+// 		}
+// 		else {
+// 			visibleWord += '_';
+// 			textNode = document.createTextNode('_');
+// 		}
+
+// 		console.log('visibleWord: ' + visibleWord);
+
+// 		//
+// 		let guessed = document.createElement('div');
+// 		guessed.className = 'word';
+// 		guessed.appendChild(textNode);
+// 		guessContent.appendChild(guessed);
+// 	}
+
+// 	guessContainer.append(guessContent);
+// }
+
+
+
+// }
