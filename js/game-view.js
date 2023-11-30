cac6f082e06be58e41d593037c274164b317fce5
Anders Hofsten <anders.jakobsen.hofsten@gmail.com>
2023-11-30 22:08:10 +0100

    game properly ends when you guess wrong 6 times. Also some styling.

--- js/game-view.js
+++ js/game-view.js
@@ -1,28 +1,29 @@
-import { alfabetet } from './svenska-ord.js';
-import { words } from './svenska-ord.js';
-import { newUserObject } from './start-view.js';
+import { alfabetet } from "./svenska-ord.js";
+import { words } from "./svenska-ord.js";
+import { newUserObject } from "./start-view.js";
 
 // display containers
-const gameViewSection = document.querySelector('.game-view-section');
-const hangingMan = document.querySelector('.hanging-man');
-const hangmanBody = ['#ground', '#scaffold', '#legs', '#arms', '#body', '#head'];
+const gameViewSection = document.querySelector(".game-view-section");
+const scoreViewSection = document.querySelector(".score-view-section");
+
+const hangingMan = document.querySelector(".hanging-man");
+const hangmanBody = ["#ground", "#scaffold", "#legs", "#arms", "#body", "#head"];
 
 // const letterContainer = document.createElement('div');
 const numberOfLetters = 10;
 const currentWord = pickNewWord(numberOfLetters);
 // const newUserObject = localStorage.getItem('newUserObject')
 
-let svgElement = document.querySelector('.hanging-man');
+let svgElement = document.querySelector(".hanging-man");
 
-let visibleWord = Array(currentWord.length).fill('_'); // initialize with underscores
+let visibleWord = Array(currentWord.length).fill("_"); // initialize with underscores
 let incorrectGuesses = 0;
 let match = false;
 
 // gameplay variables
-let wordContainer = createNewElement('div', 'word-container');
-let letterContainer = createNewElement('div', 'letter-container');
-letterContainer.className = 'letter-container';
-
+let wordContainer = createNewElement("div", "word-container");
+let letterContainer = createNewElement("div", "letter-container");
+letterContainer.className = "letter-container";
 
 // ------------------------------------------------------------------------ //
 
@@ -32,27 +33,25 @@ newGame(newUserObject);
 
 // game logic functions
 export function newGame(newUserObject) {
-
+	incorrectGuesses = 0;
 	renderAlphabet(alfabetet, letterContainer);
 	renderWord(visibleWord);
 	console.log(currentWord);
-	console.log('New Game started with difficulty level: ' + newUserObject.difficulty);
+	console.log("New Game started with difficulty level: " + newUserObject.difficulty);
+	console.log('incorrect guesses: ' + incorrectGuesses);
 }
 
 function renderAlphabet(alfabetet, letterContainer) {
-
-
 	// we first need to wipe the alphabet container clean whenever we make a new game
 	while (letterContainer.firstChild) {
-		console.log('removing child' + letterContainer.firstChild);
+		console.log("removing child" + letterContainer.firstChild);
 		letterContainer.removeChild(letterContainer.firstChild);
 	}
 
 	// loop through each char in currentWord, append the text node to our newly created element
 	for (let char of alfabetet) {
-
-		let characterContainer = createNewElement('div', 'character-container');
-		let character = createNewElement('div', 'character');
+		let characterContainer = createNewElement("div", "character-container");
+		let character = createNewElement("div", "character");
 		let textNode = document.createTextNode(char.toUpperCase());
 
 		characterContainer.appendChild(character);
@@ -60,26 +59,24 @@ function renderAlphabet(alfabetet, letterContainer) {
 		letterContainer.appendChild(character);
 
 		// add the event listener to the character
-		character.addEventListener('click', function () {
+		character.addEventListener("click", function () {
 			this.remove();
 			handleGuess(character);
 			renderWord(visibleWord);
 		});
-
 	}
 	gameViewSection.append(letterContainer);
 }
 
 function renderWord(visibleWord) {
-
 	// Every time this runs we need to clear the previous wordContainer so we don't render double words
 	while (wordContainer.firstChild) {
 		wordContainer.removeChild(wordContainer.firstChild);
 	}
 
-	// 
+	//
 	for (let char of visibleWord) {
-		let charElement = createNewElement('div', 'character-word');
+		let charElement = createNewElement("div", "character-word");
 		let textNode = document.createTextNode(char.toUpperCase());
 		charElement.appendChild(textNode);
 		wordContainer.appendChild(charElement);
@@ -89,11 +86,10 @@ function renderWord(visibleWord) {
 	gameViewSection.append(wordContainer);
 }
 
-
 function handleGuess(character) {
 	let match = false;
 	let guessedChar = character.innerText;
-	let newVisibleWord = '';
+	let newVisibleWord = "";
 	for (let i = 0; i < currentWord.length; i++) {
 		if (currentWord[i].toUpperCase() === guessedChar.toUpperCase()) {
 			newVisibleWord += currentWord[i].toUpperCase();
@@ -109,23 +105,27 @@ function handleGuess(character) {
 		let svgPart = document.querySelector(svgPartId);
 		// console.log('incorrect guesses: ' + incorrectGuesses);
 		// Make the SVG part visible
-		svgPart.classList.remove('hidden');
+		svgPart.classList.remove("hidden");
 		// console.log('class removed: ' + svgPartId);
 
 		// Increment the counter
 		incorrectGuesses++;
-	}
+		console.log("incorrect guesses: " + incorrectGuesses);
 
-	else {
-		gameOver(newUserObject);
+		if (incorrectGuesses === 6) {
+			gameOver(newUserObject);
+		}
 	}
 
 	visibleWord = newVisibleWord;
 	renderWord(visibleWord);
 }
 export function gameOver(newUserObject) {
-
-	console.log('Game over');
+	incorrectGuesses = 0;
+	// startViewSection.classList.add('hidden');
+	gameViewSection.classList.add("hidden");
+	scoreViewSection.classList.remove("hidden");
+	console.log("Game over");
 
 	// when you suck, ame ends. Do stuff here
 }
@@ -138,9 +138,8 @@ function createNewElement(typeOfElement, className) {
 }
 
 function pickNewWord(numberOfLetters) {
-
 	// find the ten letter letters from the list by filtering
-	const currentWordList = words.filter(word => word.length === numberOfLetters);
+	const currentWordList = words.filter((word) => word.length === numberOfLetters);
 
 	/* generate random float based on length of currentWordList,
 	then floor it to an integer.
@@ -151,4 +150,3 @@ function pickNewWord(numberOfLetters) {
 	// then return the word that we got from the array
 	return currentWordList[newWord];
 }
-
